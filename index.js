88fb3fcb286fff171a81f10e250551d3b89c16eb
Julio César Da Silva <jnb.py13@hotmail.com>
2023-07-26 16:54:10 -0300

    Adicionado o comando ip

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 // Importar as bibliotecas necessárias
 const { Telegraf } = require('telegraf'); //Biblioteca Telegraf para o bot do Telegram
 const puppeteer  = require('puppeteer'); //Biblioteca Puppeteer para web scraping
-const bot = new Telegraf("TOKEM_BOT_TELEGRAM");//Token do boot telegram
+const bot = new Telegraf("TOKEN_BOT_TELEGRAM"); //Token do boot telegram
+let ip;// Variável para armazenar o endereço IP
 
-async function navegador(){ // Biblioteca Puppeteer para web scraping
+async function navegador(){ // Função para configurar e executar o Puppeteer para web scraping
   const browser = await puppeteer.launch({headless: true}); // Iniciar uma instância do navegador Chromium em modo "headless" (sem interface gráfica)
   const page = await browser.newPage(); // Criar uma nova página no navegador
   await page.goto('http://127.0.0.1:4040');// Navegar para a URL especificada
-  const f = await page.$("[class='tunnels']"); // Encontrar um elemento com a classe CSSx'
-  const text = await (await f.getProperty('textContent')).jsonValue();// Obter o conteúdo de texto desse elemento
-  textenavegador = text; // Atribuir o valor do texto à variável 'textenavegador'
+  const classcss = await page.$("[class='tunnels']"); // Encontrar um elemento com a classe CSS 'tunnels'
+  const text = await (await classcss.getProperty('textContent')).jsonValue();// Obter o conteúdo de texto desse elemento
+  ip = text;// Armazenar o endereço IP da variável 'text' e colocando na variável 'ip'
   await browser.close();// Fechar o navegador
 }
-navegador()// Chamar a função 'navegador'
-  .then(() => {// Enviar a mensagem com o conteúdo extraído para o grupo ou chat com o ID '-1001816074750'
-    bot.telegram.sendMessage(-1001816074750, `${textenavegador}`);
+navegador()//Chamar a função 'navegador' para obter o endereço IP
+  .then(() => {
+    bot.telegram.sendMessage(chat_ID, `${ip}`); // Enviar o endereço IP obtido para o chat_ID especificado
   })
   .catch((error) => {
-    console.error('Error occurred:', error);
+    console.error('Error occurred:', error); // Tratar erros, caso ocorram
   });
+
+bot.command('ip', (ctx) => ctx.reply(`${ip}`)); // Comando para responder com o endereço IP ao receber o comando "/ip"
+bot.launch(); // Iniciar o bot Telegram
--- readme.md
+++ readme.md
@@ -1,10 +1,33 @@
-### Sobre
-Este é meu primeiro bot para o telegram feito em javascript e meu primeiro código em javascript, ele foi criado para pegar o IP do **[ngrok](https://ngrok.com)**
-### Como instalar 
+<h1 align="center">Sobre</h1>
+Este é meu primeiro bot para o telegram feito em javascript e meu primeiro código em javascript, ele foi criado para pegar o IP do <a href="https://ngrok.com" target="_blank">ngrok</a>
+
+<div align="center">
+<h2>TELA DO NGROK</h2>
+<img src="https://media.discordapp.net/attachments/1013591689715261502/1133847699683283076/image-2.png?width=900&height=569" width="500px"/>
+</div>
+<div align="center">
+<h2>TELEGRAM</h2>
+<img src="https://media.discordapp.net/attachments/1013591689715261502/1133847699951714334/image.png?width=716&height=569" width="500px"/>
+</div>
+
+<h1 align="center">Como instalar</h1>
+
+<h3>Com interface gráfica</3>
 
 ```bash
 $ git clone https://github.com/JulionBR/Primeiro_bot_telegram.git
 $ cd Primeiro_bot_telegram/
+$ npm install
+$ node index.js
+```
+
+<h3>Sem interface gráfica</3>
+
+```bash
+$ git clone https://github.com/JulionBR/Primeiro_bot_telegram.git
+$ cd Primeiro_bot_telegram/
+$ chmod +x RodaNoServer.sh
+$ ./RodaNoServer.sh
 $ npm install
 $ node index.js
 ```
